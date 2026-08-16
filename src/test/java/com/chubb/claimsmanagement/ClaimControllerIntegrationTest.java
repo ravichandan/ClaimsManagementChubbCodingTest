7bc0073c0f781d8e7ecea52305435406d7b3a663
“Chandan <s.ravichandan@gmail.com>
2026-08-16 11:28:28 +1000

    gradle build working

--- src/test/java/com/chubb/claimsmanagement/ClaimControllerIntegrationTest.java
+++ src/test/java/com/chubb/claimsmanagement/ClaimControllerIntegrationTest.java
@@ -3,7 +3,6 @@ package com.chubb.claimsmanagement;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
@@ -14,7 +13,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
-@AutoConfigureMockMvc
+@org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc
 class ClaimControllerIntegrationTest {
 
     @Autowired
